418dc156b0a5516f640639d2b4ade6e2fa06c80d
DarkNightRise <kashyap.ankit2am@gmail.com>
2021-06-28 11:37:15 +0530

    Creating Employee Class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,20 @@
+class Employee
+{
+	private String fname;
+	private String lname;
+
+	public Employee(String fname, String lname){
+		this.fname=fname;
+		this.lname=lname;
+	}
+	public void display(){
+		System.out.println("Your first name is: "+this.fname+" and last name is: "+this.lname);
+	}
+}
 public class HelloWorld
 {
 	public static void main(String... args){
 		System.out.println("Hello, Jenkins Users");
+		new Employee("Jake", "Harper").display();
 	}
 }
